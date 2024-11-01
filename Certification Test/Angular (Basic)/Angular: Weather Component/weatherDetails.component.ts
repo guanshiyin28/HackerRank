5782f94fafcbc9aaadcbacabfd1fcd205c69b4bf
Guan <134080362+guanshiyin28@users.noreply.github.com>
2024-11-02 00:40:32 +0700

    Update weatherDetails.component.ts

--- Certification Test/Angular (Basic)/Angular: Weather Component/weatherDetails.component.ts
+++ Certification Test/Angular (Basic)/Angular: Weather Component/weatherDetails.component.ts
@@ -1,25 +1,24 @@
-import {Component, Input, OnInit} from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'weather-details',
   templateUrl: './weatherDetails.component.html',
   styleUrls: ['./weatherDetails.component.scss']
 })
-
 export class WeatherDetails implements OnInit {
   @Input() weatherData: data[];
-  city: string;
-  cityWeather: data;
+  city: string = '';
+  cityWeather: data | undefined;
 
   ngOnInit() {
-
+    // Initialize any necessary data here if needed
   }
 
-  getCityWeather(event: Event) {
+  onCityInput(event: Event) {
     this.city = (<HTMLInputElement>event.target).value;
-    this.cityWeather = this.weatherData.find((weather) => {
-      return weather.name.toLowerCase() === this.city.toLowerCase();
-    });
+    this.cityWeather = this.weatherData.find(weather =>
+      weather.name.toLowerCase() === this.city.toLowerCase()
+    );
   }
 }
 
