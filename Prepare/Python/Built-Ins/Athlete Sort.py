edb5bc18b99e46c519e716a042f755f98d85865e
Guan <134080362+guanshiyin28@users.noreply.github.com>
2024-11-02 14:57:29 +0700

    Create Athlete Sort.py

---
+++ Prepare/Python/Built-Ins/Athlete Sort.py
@@ -0,0 +1,21 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    nm = input().split()
+    n = int(nm[0])
+    m = int(nm[1])
+    arr = []
+    for _ in range(n):
+        arr.append(list(map(int, input().split())))
+    k = int(input())
+    arr = sorted(arr, key = lambda x: x[k])
+    for x in arr:
+        print(' '.join(map(str, x)))
