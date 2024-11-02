a5d2ec4c470d1424540e9755660e9f6a8f196c52
Guan <134080362+guanshiyin28@users.noreply.github.com>
2024-11-02 15:08:35 +0700

    Create Detect HTML Tags, Attributes and Attribute Values.py

---
+++ Prepare/Python/Regex and Parsing/Detect HTML Tags, Attributes and Attribute Values.py
@@ -0,0 +1,10 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from html.parser import HTMLParser
+class MyHTMLParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        print(tag)
+        for x, y in attrs:
+            print(f"-> {x} > {y}")
+t = ''.join(input() for _ in range(int(input())))
+parser = MyHTMLParser()
+parser.feed(t)
