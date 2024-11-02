fbfe2a46e161d98d1bc752c60a7f7e4a96443326
Guan <134080362+guanshiyin28@users.noreply.github.com>
2024-11-02 15:13:29 +0700

    Create Standardize Mobile Number Using Decorators.py

---
+++ Prepare/Python/Closures and Decorators/Standardize Mobile Number Using Decorators.py
@@ -0,0 +1,15 @@
+from re import sub
+def wrapper(f):
+    def fun(l):
+        return f([sub(r"(.*){0,3}(\d{5})(\d{5})$",\
+        r"+91 \2 \3", num)for num in l])
+    return fun
+@wrapper
+def sort_phone(l):
+    print(*sorted(l), sep='\n')
+
+if __name__ == '__main__':
+    l = [input() for _ in range(int(input()))]
+    sort_phone(l) 
+
+
