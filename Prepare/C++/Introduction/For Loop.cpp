c2913f576804fdcedb61dd5f34b19f91be1130ea
Guan <134080362+guanshiyin28@users.noreply.github.com>
2024-11-03 00:42:23 +0700

    Update For Loop.cpp

--- Prepare/C++/Introduction/For Loop.cpp
+++ Prepare/C++/Introduction/For Loop.cpp
@@ -2,50 +2,21 @@
 #include <cstdio>
 using namespace std;
 
-int main() { int a, b; cin >> a >> b;
+int main() {
+    int a, b;
+    cin >> a >> b;
+        
+    string numbers[] = {"one", "two", "three", "four", "five", 
+                        "six", "seven", "eight", "nine"};
 
-for (int i = a; i <= 9; i++) {
-    switch (i) {
-        case 1:
-            cout << "one" << endl;
-            break;
-        case 2:
-            cout << "two" << endl;
-            break;
-        case 3:
-            cout << "three" << endl;
-            break;
-        case 4:
-            cout << "four" << endl;
-            break;
-        case 5:
-            cout << "five" << endl;
-            break;
-        case 6:
-            cout << "six" << endl;
-            break;
-        case 7:
-            cout << "seven" << endl;
-            break;
-        case 8:
-            cout << "eight" << endl;
-            break;
-        case 9:
-            cout << "nine" << endl;
-            break;
-        default:
-            break;
+    for (int i = a; i <= b; i++) {
+        if (i >= 1 && i <= 9) {
+            cout << numbers[i - 1] << endl;
+        } else if (i % 2 == 0) {
+            cout << "even" << endl;
+        } else {
+            cout << "odd" << endl;
+        }
     }
-}
-
-for (int j = 10; j <= b; j++) {
-    if (j % 2 == 0) {
-        cout << "even" << endl;
-    } else {
-        cout << "odd" << endl;
-    }
-}
-
-return 0;
-
+    return 0;
 }
