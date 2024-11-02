e4c49b30fb0304d79888c072532f659700ab0aef
Guan <134080362+guanshiyin28@users.noreply.github.com>
2024-11-02 13:49:30 +0700

    Create Collections.OrderedDict().py

---
+++ Prepare/Python/Collections/Collections.OrderedDict().py
@@ -0,0 +1,7 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from collections import OrderedDict
+orders = OrderedDict()
+for i in range(int(input())):
+    name, _, price = input().rpartition(' ')
+    orders[name] = orders.get(name, 0) + int(price)
+[print(*item) for item in orders.items()]
