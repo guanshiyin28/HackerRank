80a984c9087702e61493e8913f16413a2170e82a
Guan <134080362+guanshiyin28@users.noreply.github.com>
2024-11-04 21:18:17 +0700

    Create The Hurdle Race.py

---
+++ Prepare/Problem Solving/Implementation/The Hurdle Race.py
@@ -0,0 +1,47 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'hurdleRace' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER k
+#  2. INTEGER_ARRAY height
+#
+
+def hurdleRace(k, height):
+    n, cont, aux = len(height), 0, 0
+    while(n > 0):
+        if k < height[aux]:
+            k +=1
+            cont +=1
+        else:
+            n-=1
+            aux+=1
+
+
+    return cont
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    height = list(map(int, input().rstrip().split()))
+
+    result = hurdleRace(k, height)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
