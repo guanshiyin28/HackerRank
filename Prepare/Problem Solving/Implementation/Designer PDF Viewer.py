3f812672ecaf3c3d4dc1d21f69d0f47b9a28cb7c
Guan <134080362+guanshiyin28@users.noreply.github.com>
2024-11-04 21:20:14 +0700

    Create Designer PDF Viewer.py

---
+++ Prepare/Problem Solving/Implementation/Designer PDF Viewer.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'designerPdfViewer' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY h
+#  2. STRING word
+#
+
+def designerPdfViewer(h, word):
+    # Write your code here
+    letters = 'abcdefghijklmnopqrstuvwxyz'
+    width = len(word)
+    index_list = [letters.index(x) for x in word]
+    length = max([h[x] for x in index_list])
+    return width*length
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    h = list(map(int, input().rstrip().split()))
+
+    word = input()
+
+    result = designerPdfViewer(h, word)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
