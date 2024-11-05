64c28bcf2f1f87cbd96e2013d9112b791be23186
Guan <134080362+guanshiyin28@users.noreply.github.com>
2024-11-05 10:00:45 +0700

    Create Beautiful Triplets.py

---
+++ Prepare/Problem Solving/Implementation/Beautiful Triplets.py
@@ -0,0 +1,44 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'beautifulTriplets' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER d
+#  2. INTEGER_ARRAY arr
+#
+
+def beautifulTriplets(d, arr):
+    count = 0
+    set_of_numbers = set(arr)
+    for n in arr:
+        fir = n
+        sec = fir + d
+        thi = sec + d
+        if sec in set_of_numbers and thi in set_of_numbers:
+            count += 1
+    return count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    d = int(first_multiple_input[1])
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = beautifulTriplets(d, arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
